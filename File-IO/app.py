49b4d4ed080c91a7c44b448d818e685eb3e79d04
Tyler Nenninger <tylernenninger@Annas-Air.lan>
2022-11-15 11:16:28 -0600

    fileio

---
+++ File-IO/app.py
@@ -0,0 +1,23 @@
+# file = open('file.text', 'r')
+
+# f = file.readlines()
+
+# newList = []
+# for line in f:
+#     newList.append(line.strip())
+
+# print(newList)
+
+# file = open('file2.txt', 'w')
+
+# file.write('python\n')
+# file.write('tyler')
+
+# file.close()
+
+# .find(), .count()
+
+string = 'hello'
+
+print(string.find('l'))
+
